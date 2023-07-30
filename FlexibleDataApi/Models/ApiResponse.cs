cb0386d87298c574ffe260962ad913c39358920b
Muljayan <37667437+Muljayan@users.noreply.github.com>
2023-07-31 00:36:03 +0530

    initial endpoint structure and response model

---
+++ FlexibleDataApi/Models/ApiResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net;
+
+namespace FlexibleDataApi.Models
+{
+    public class ApiResponse
+    {
+        public bool ISuccess { get; set; }
+        public Object Result { get; set; }
+        public HttpStatusCode StatusCode { get; set; }
+        public List<string> ErrorMessages { get; set; }
+        public ApiResponse()
+        {
+            ErrorMessages = new List<string>();
+        }
+    }
+}
+
--- FlexibleDataApi/Program.cs
+++ FlexibleDataApi/Program.cs
@@ -1,4 +1,7 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using System.Net;
+using FlexibleDataApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -16,11 +19,38 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapGet("/", () =>
-{
 
-    return "Hello World";
+var fdApp = app.MapGroup("/flexibledata");
+
+fdApp.MapPost("/create", () =>
+{
+    var response = new ApiResponse();
+    response.Result = new { id = 1 };
+    response.ISuccess = true;
+    response.StatusCode = HttpStatusCode.Created;
+    return Results.CreatedAtRoute("GetFlexibleData", new { id = 1 }, response);
 })
-.WithName("HelloWorld");
+.WithName("CreateFlexibleData");
+
+fdApp.MapGet("/get/{id:int}", (int id) =>
+{
+    var response = new ApiResponse();
+    response.Result = new { id };
+    response.ISuccess = true;
+    response.StatusCode = HttpStatusCode.OK;
+    return Results.Ok(response);
+})
+.WithName("GetFlexibleData");
+
+fdApp.MapGet("/get/{key:alpha}", (string key) =>
+{
+    var response = new ApiResponse();
+    response.Result = new { key };
+    response.ISuccess = true;
+    response.StatusCode = HttpStatusCode.OK;
+    return Results.Ok(response);
+})
+.WithName("GetKeyCount");
+
 
 app.Run();
